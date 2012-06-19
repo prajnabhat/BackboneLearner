9df5227dc128a4bab322b265d6a398532ae1d3f0
Sreechand Tavva <sreechandt@gmail.com>
2012-06-19 15:45:16 +0530

    Comments and Styling

--- js/app1.js
+++ js/app1.js
@@ -1,20 +1,23 @@
 (function($){
 var contacts = [
 { name : "Prajna", age: 21  },
-{ name : "Sreechand", age : 25 },
+{ name : "Sreechand", age : 23 },
 { name : "Harsha", age : 20}
 ];
 
+//Creating a model "Contact" with a default value for Company
 var Contact = Backbone.Model.extend({
 defaults : {
 Company : 'Artoo'
 }
 });
 
+//Creating a list of "Contacts"
 var List = Backbone.Collection.extend({
 model : Contact
 });
 
+// A view to display a single 
 var ContactView = Backbone.View.extend({
 //tagname : "article",
 //className : "contact-container",
@@ -26,7 +29,7 @@ render : function(){
 	return this;
 }
 });
-
+// A view for displaying all contacts
 var ListView = Backbone.View.extend({
 el : $("#contacts"),
 initialize : function(){
